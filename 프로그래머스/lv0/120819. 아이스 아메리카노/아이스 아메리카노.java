ccc6c8facaba69a7e663cfac8a4ea6dbb58f4483
code_moon <97062552+moonwonki@users.noreply.github.com>
2023-06-02 16:38:59 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] solution(int money) {
-        int[] answer = new int[] {money / 5500, money % 5500};
-        return answer;
+        return new int[] {money / 5500, money % 5500};
     }
 }
