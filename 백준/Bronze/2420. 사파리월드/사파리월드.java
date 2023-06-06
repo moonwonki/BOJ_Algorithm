9c87eb1203601402f95d10ce88c3ccd239f5b144
code_moon <97062552+moonwonki@users.noreply.github.com>
2023-06-06 13:13:06 +0900

    [Bronze V] Title: 사파리월드, Time: 208 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        long a = scn.nextLong();
+        long b = scn.nextLong();
+
+        System.out.println(Math.abs(a - b));
+    }
+}
