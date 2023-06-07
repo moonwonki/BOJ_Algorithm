3bad3521cbf65835b759444604577f197a330798
code_moon <97062552+moonwonki@users.noreply.github.com>
2023-06-08 00:00:12 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 116 ms, Memory: 13824 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println("       _.-;;-._\n" +
+				"'-..-'|   ||   |\n" +
+				"'-..-'|_.-;;-._|\n" +
+				"'-..-'|   ||   |\n" +
+				"'-..-'|_.-''-._|");
+    }
+}
