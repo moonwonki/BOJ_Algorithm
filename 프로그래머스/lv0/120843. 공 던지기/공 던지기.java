be7ec7660aca0e3730cc84ef25cec6e3540be590
code_moon <97062552+moonwonki@users.noreply.github.com>
2023-06-14 18:00:01 +0900

    [level 0] Title: 공 던지기, Time: 0.04 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
@@ -1,11 +1,15 @@
 class Solution {
     public int solution(int[] numbers, int k) {
         int answer = 0;
+        
+        //던지는 횟수가 1이면 첫번째 사람이 던지는 사람이기 때문에 바로 1 리턴.
         if (k == 1) return 1;
         
+        //전체 사람 수
         int people = numbers.length;
-        
+        //사람을 건너가는 횟수
         int toss = (k - 1) * 2;
+        //답 구하기
         answer = toss % people + 1;
         return answer;
     }
