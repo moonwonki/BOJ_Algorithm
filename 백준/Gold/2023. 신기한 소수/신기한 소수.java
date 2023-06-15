a5e27067d9f8b7f98b1a5168ef3edb3465579958
code_moon <97062552+moonwonki@users.noreply.github.com>
2023-06-15 16:59:37 +0900

    [Gold V] Title: 신기한 소수, Time: 224 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -0,0 +1,71 @@
+
+import java.util.*;
+
+public class Main {
+    static ArrayList<Integer> strangePrime;
+    static boolean[] notPrimeList;
+    static double limit;
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int N = scn.nextInt();
+        strangePrime = new ArrayList<>();
+        /*
+        limit = 0;
+        for (int i = 0; i < N; i++){
+            limit *= 10;
+            limit += 9;
+        }
+
+        limit = Math.sqrt(limit);
+        notPrimeList = new boolean[(int) limit + 2];
+
+        fillPrimeList();
+        notPrimeList[1] = true;
+        */
+
+
+        dfs(N, 0, 0);
+
+        for (int i = 0; i < strangePrime.size(); i++){
+            System.out.println(strangePrime.get(i));
+        }
+    }
+
+    public static void dfs(int N, int number, int count){
+        if (!checkPrime(number)){
+            return;
+        }
+
+        if (count == N) {
+            strangePrime.add(number);
+            return;
+        }
+
+        for (int i = 1; i <= 9; i ++){
+            if (count == 0 && i == 1) continue;
+            dfs(N, number * 10 + i, count + 1);
+        }
+    }
+
+    private static boolean checkPrime(int number) {
+        for (int i = 2; i <= Math.sqrt(number); i++){
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void fillPrimeList(){
+        for (int i = 2; i <= limit; i++){
+            int num = 2;
+            while (i * num <= limit){
+                notPrimeList[i * num] = true;
+                num++;
+            }
+        }
+    }
+
+
+
+}
