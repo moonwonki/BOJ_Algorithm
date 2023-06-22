2232a196bb0744a55943b99c10aa37704fd7678f
code_moon <97062552+moonwonki@users.noreply.github.com>
2023-06-22 10:10:15 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = new int[2];
+        
+        int max = 0;
+        int maxIdx = -1;
+        
+        for (int i = 0; i < array.length; i++){
+            if (array[i] > max) {
+                max = array[i];
+                maxIdx = i;
+            }
+        }
+        
+        answer[0] = max;
+        answer[1] = maxIdx;
+        return answer;
+    }
+}
