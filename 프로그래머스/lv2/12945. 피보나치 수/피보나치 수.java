99c68d0dce612c91dd1a579f610ce1042082a312
code_moon <97062552+moonwonki@users.noreply.github.com>
2023-07-17 17:29:44 +0900

    [level 2] Title: 피보나치 수, Time: 1.67 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -1,19 +1,20 @@
 class Solution {
-    static int dp[];
-    
     public int solution(int n) {
         int answer = 0;
         
-        int nminusone = 1;
-        int nminustwo = 0;
+        int nminusone = 1;//F(n-1)
+        int nminustwo = 0;//F(n-2)
+        
+        //F(n) = nminusone + nminustwo
+        //nminusone을 바꿔줌. n++이 일어나니까.
+        //nminustwo을 바꿔줌. n++이 일어나니까
         
         for (int index = 2; index <= n; index++){
-            answer = (nminusone + nminustwo) % 1234567;
+            answer = (nminusone + nminustwo) % 1234567;//결과에 큰 차이가 없다!!
             nminustwo = nminusone;
             nminusone = answer;
         }
-        
-        
+            
         return answer;
     }
 }
